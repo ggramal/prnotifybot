8828e648d57f42c931e4e78dc16d483c714d7361
Gramovich Alexander <gramovich.alex@outlook.com>
2023-05-14 02:03:43 +0300

    Implement pr message update logic

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, abort, make_response, request
 from github_webhook import Webhook
 from dataclasses import dataclass, fields
+from collections import UserDict
+from datetime import datetime
 
 app = Flask(__name__)
 webhook = Webhook(app,endpoint="/webhooks/pr")
+pr_dict = {}
+now = datetime.now()
+now = now.strftime("%Y-%m-%d %H:%M:%S")
 
 
 @dataclass(init=False)
@@ -23,12 +28,20 @@ class GitHubUser(GitHubEntity):
     def __str__(self):
         return f"{self.login}"
 
-@dataclass(init=False)
+@dataclass(init=False, eq=False)
 class PullRequest(GitHubEntity):
     user: GitHubUser
     state: str
     title: str
     merged_at: str
+
+    def __eq__(self, other):
+        if isinstance(other, PullRequest):
+            return self.html_url == other.html_url
+        return NotImplemented
+
+    def __hash__(self):
+        return  hash((self.html_url))
 
     def __str__(self):
         emoji = ""
@@ -40,14 +53,16 @@ class PullRequest(GitHubEntity):
         elif self.state == "open":
             emoji = " :sparkles:"
 
-        if self.state == "pushed": return f"{self.user} {self.state}"
+        if self.state == "pushed":
+            return now + f" {self.user} pushed"
 
 
-        return f"[{self.state}{emoji}] {self.title}\n{self.user} {self.html_url}"
+        return f"[{self.state}{emoji}] {self.title} {self.html_url}"
 
 @dataclass(init=False)
 class PullRequestReview(GitHubEntity):
     user: GitHubUser
+    pull_request: PullRequest
     state: str
 
     def __str__(self):
@@ -55,15 +70,25 @@ class PullRequestReview(GitHubEntity):
         if self.state == "approved":
             emoji = " :heavy_check_mark:"
 
-        return f"{self.user} {self.state}{emoji}"
+        return now + f" {self.user} {self.state}{emoji}"
 
 @dataclass(init=False)
 class PullRequestReviewComment(GitHubEntity):
     user: GitHubUser
+    pull_request: PullRequest
     state: str
 
     def __str__(self):
-        return f"{self.user} {self.state} comment {self.html_url}"
+        return now + f" {self.user} {self.state} comment {self.html_url}"
+
+@dataclass(init=False)
+class IssueComment(GitHubEntity):
+    user: GitHubUser
+    pull_request: PullRequest
+    state: str
+
+    def __str__(self):
+        return now + f" {self.user} {self.state} issue comment {self.html_url}"
 
 
 
@@ -75,24 +100,88 @@ def get_pull_request(data):
     data = data["pull_request"]
     data["user"] = GitHubUser(**data["user"])
     pr = PullRequest(**data)
-    print(pr)
+    message = f"{pr}"
+    if pr in pr_dict.keys():
+        if pr.state == "pushed":
+            message = f"{pr_dict[pr]}\n{pr}"
+        else:
+            message_lines = pr_dict[pr].split('\n')
+            pr_lines = str(pr).split('\n')
+            message_lines[0] = pr_lines[0]
+            message = '\n'.join(message_lines)
+
+    pr_dict[pr] = message
+    print(pr_dict[pr])
     return {"status":"ok"}
 
 @webhook.hook("pull_request_review")
 def get_pull_request_review(data):
+    user = GitHubUser(**data["review"]["user"])
+
+    data["review"]["user"] = user
+    data["pull_request"]["user"] = user
+
+    pr = PullRequest(**data["pull_request"])
+
     data = data["review"]
-    data["user"] = GitHubUser(**data["user"])
+    data["pull_request"] = pr
+
     pr_review = PullRequestReview(**data)
-    print(pr_review)
+
+    if pr_review.state == "commented": return {"status": "ok"}
+
+    if pr in pr_dict.keys():
+        pr_dict[pr] = f"{pr_dict[pr]}\n{pr_review}"
+    else:
+        pr_dict[pr] = f"{pr}\n{pr_review}"
+
+    print(pr_dict[pr])
     return {"status":"ok"}
 
 @webhook.hook("pull_request_review_comment")
 def get_pull_request_review_comment(data):
+    user = GitHubUser(**data["comment"]["user"])
+
     data["comment"]["state"] = data["action"]
+    data["comment"]["user"] = user
+    data["pull_request"]["user"] = user
+
+    pr = PullRequest(**data["pull_request"])
+
     data = data["comment"]
-    data["user"] = GitHubUser(**data["user"])
+    data["pull_request"] = pr
+
     pr_review_comment = PullRequestReviewComment(**data)
-    print(pr_review_comment)
+
+    if pr in pr_dict.keys():
+        pr_dict[pr] = f"{pr_dict[pr]}\n{pr_review_comment}"
+    else:
+        pr_dict[pr] = f"{pr}\n{pr_review_comment}"
+    print(pr_dict[pr])
+    return {"status":"ok"}
+
+@webhook.hook("issue_comment")
+def get_issue_comment(data):
+    user = GitHubUser(**data["comment"]["user"])
+
+    data["comment"]["state"] = data["action"]
+    data["comment"]["user"] = user
+    data["issue"]["pull_request"]["user"] = user
+    data["issue"]["pull_request"]["state"] = None
+    data["issue"]["pull_request"]["title"] = None
+
+    pr = PullRequest(**data["issue"]["pull_request"])
+
+    data = data["comment"]
+    data["pull_request"] = pr
+
+    issue_comment = IssueComment(**data)
+
+    if pr in pr_dict.keys():
+        pr_dict[pr] = f"{pr_dict[pr]}\n{issue_comment}"
+    else:
+        pr_dict[pr] = f"{pr}\n{issue_comment}"
+    print(pr_dict[pr])
     return {"status":"ok"}
 
 
